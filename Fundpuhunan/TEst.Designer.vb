12615589c68596d98848d72e088983daf8969416
Allan Camarines <lan.camarines@yahoo.com>
2022-03-10 11:22:57 +0800

    test

--- Fundpuhunan/TEst.Designer.vb
+++ Fundpuhunan/TEst.Designer.vb
@@ -22,9 +22,17 @@ Partial Class TEst
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'TEst
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "TEst"
         Me.Text = "TEst"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
