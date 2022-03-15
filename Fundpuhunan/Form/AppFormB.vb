48261977a29f66511fbfcf88b1e5f96def40855d
RaymartSalaysay <salaysayraymartbs@gmail.com>
2022-03-15 11:38:30 +0800

    update_again

--- Fundpuhunan/Form/AppFormB.vb
+++ Fundpuhunan/Form/AppFormB.vb
@@ -56,4 +56,78 @@
             Button2.Text = OpenFileDialog3.FileName
         End If
     End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+
+
+
+
+        Dim Item As New ListViewItem(TextBox12.Text)
+
+
+        Item.SubItems.Add(TextBox13.Text)
+        Item.SubItems.Add(TextBox14.Text)
+        Item.SubItems.Add(TextBox15.Text)
+
+        ListView1.Items.Add(Item)
+
+
+
+
+
+    End Sub
+
+    Private Sub TextBox14_TextChanged(sender As Object, e As EventArgs) Handles TextBox14.TextChanged
+
+
+
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+        Dim Item As New ListViewItem(TextBox28.Text)
+
+
+        Item.SubItems.Add(TextBox26.Text)
+        Item.SubItems.Add(TextBox27.Text)
+        Item.SubItems.Add(TextBox28.Text)
+
+        ListView2.Items.Add(Item)
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        ListView1.Items.RemoveAt(ListView1.SelectedIndices(0))
+    End Sub
+
+    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub ListView1_MouseClick(sender As Object, e As MouseEventArgs) Handles ListView1.MouseClick
+
+
+
+
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        ListView2.Items.RemoveAt(ListView2.SelectedIndices(0))
+    End Sub
+
+    Private Sub ListView2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView2.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub ListView2_MouseClick(sender As Object, e As MouseEventArgs) Handles ListView2.MouseClick
+
+    End Sub
 End Class
