aef2b94d964a271fc306f474abbc72f9a843eb9a
allanc0304 <lan.camarines@gmail.com>
2022-03-15 14:55:11 +0800

    van dam

--- Fundpuhunan/Form/CIRFORM1.vb
+++ Fundpuhunan/Form/CIRFORM1.vb
@@ -21,8 +21,7 @@ Public Class CIRFORM1
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        CIRFORM2.Show()
-        Me.Hide()
+
 
     End Sub
 
