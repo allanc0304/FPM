220cf62fc5d1986884c9b3b769c68205187072ca
Dennis <dennisacilo@gmail.com>
2022-03-17 18:07:37 +0800

    Dennis: Update

--- Fundpuhunan/LoanProcessing.vb
+++ Fundpuhunan/LoanProcessing.vb
@@ -1,4 +1,5 @@
-﻿Public Class LoanProcessing
+﻿Imports DBConnection
+Public Class LoanProcessing
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         CIRFORM1.Show()
     End Sub
@@ -14,7 +15,21 @@
     End Sub
     Private Sub DataGridView1_ButtonClick(sender As DataGridView, e As DataGridViewCellEventArgs) Handles Me.DataGridView1ButtonClick
         If e.ColumnIndex = 8 Then
-            MessageBox.Show("view")
+            Dim loanId As String = sender.Rows(e.RowIndex).Cells(0).Value
+            Dim connection = HolderSingleton.connection
+            Dim exception = SQLHelper.GetCIR(connection, HolderSingleton.selectedCIR, loanId)
+            If exception IsNot Nothing Then
+                MessageBox.Show(exception.Message)
+                Exit Sub
+            End If
+            Dim cirObj = HolderSingleton.selectedCIR
+            CIRFORM1.TextBox1.Text = cirObj.BorrowerLN
+            CIRFORM1.TextBox2.Text = cirObj.BorrowerFN
+            CIRFORM1.TextBox3.Text = cirObj.BorrowerMN
+            CIRFORM1.TextBox4.Text = cirObj.BorrowerSuffix
+
+            CIRFORM1.Show()
+            CIRFORM1.BringToFront()
         ElseIf e.ColumnIndex = 7 Then
             MessageBox.Show("delete")
         End If
