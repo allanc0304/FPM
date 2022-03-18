f5a7f0ca37d74f089c0fabd262396bdbdae3c75e
Allan Camarines <lan.camarines@yahoo.com>
2022-03-18 10:16:37 +0800

    f2

--- Fundpuhunan/Form/CIRFORM2.vb
+++ Fundpuhunan/Form/CIRFORM2.vb
@@ -2,4 +2,8 @@
     Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
 End Class
