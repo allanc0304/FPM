29c8a971694d644dc277d89f7b33b760e89e315e
RaymartSalaysay <salaysayraymartbs@gmail.com>
2022-04-05 04:52:36 +0800

    depost_update

--- Fundpuhunan/Savings/AddSavingsDeposit.Designer.vb
+++ Fundpuhunan/Savings/AddSavingsDeposit.Designer.vb
@@ -25,12 +25,6 @@ Partial Class AddSavingsDeposit
         Me.Panel2 = New System.Windows.Forms.Panel()
         Me.Label23 = New System.Windows.Forms.Label()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.Label21 = New System.Windows.Forms.Label()
-        Me.TextBox11 = New System.Windows.Forms.TextBox()
-        Me.Label22 = New System.Windows.Forms.Label()
-        Me.Label20 = New System.Windows.Forms.Label()
-        Me.TextBox10 = New System.Windows.Forms.TextBox()
-        Me.Label19 = New System.Windows.Forms.Label()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Label14 = New System.Windows.Forms.Label()
         Me.Button2 = New System.Windows.Forms.Button()
@@ -41,7 +35,6 @@ Partial Class AddSavingsDeposit
         Me.TextBox9 = New System.Windows.Forms.TextBox()
         Me.Label17 = New System.Windows.Forms.Label()
         Me.TextBox8 = New System.Windows.Forms.TextBox()
-        Me.Label13 = New System.Windows.Forms.Label()
         Me.Label12 = New System.Windows.Forms.Label()
         Me.Label11 = New System.Windows.Forms.Label()
         Me.Label10 = New System.Windows.Forms.Label()
@@ -50,10 +43,8 @@ Partial Class AddSavingsDeposit
         Me.TextBox6 = New System.Windows.Forms.TextBox()
         Me.TextBox5 = New System.Windows.Forms.TextBox()
         Me.Label8 = New System.Windows.Forms.Label()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
@@ -84,19 +75,13 @@ Partial Class AddSavingsDeposit
         Me.Label23.ForeColor = System.Drawing.Color.White
         Me.Label23.Location = New System.Drawing.Point(313, 8)
         Me.Label23.Name = "Label23"
-        Me.Label23.Size = New System.Drawing.Size(175, 24)
+        Me.Label23.Size = New System.Drawing.Size(144, 24)
         Me.Label23.TabIndex = 41
-        Me.Label23.Text = "Add Loan Payment "
+        Me.Label23.Text = "Savings Deposit"
         '
         'GroupBox1
         '
         Me.GroupBox1.BackColor = System.Drawing.Color.WhiteSmoke
-        Me.GroupBox1.Controls.Add(Me.Label21)
-        Me.GroupBox1.Controls.Add(Me.TextBox11)
-        Me.GroupBox1.Controls.Add(Me.Label22)
-        Me.GroupBox1.Controls.Add(Me.Label20)
-        Me.GroupBox1.Controls.Add(Me.TextBox10)
-        Me.GroupBox1.Controls.Add(Me.Label19)
         Me.GroupBox1.Controls.Add(Me.Button3)
         Me.GroupBox1.Controls.Add(Me.Label14)
         Me.GroupBox1.Controls.Add(Me.Button2)
@@ -107,7 +92,6 @@ Partial Class AddSavingsDeposit
         Me.GroupBox1.Controls.Add(Me.TextBox9)
         Me.GroupBox1.Controls.Add(Me.Label17)
         Me.GroupBox1.Controls.Add(Me.TextBox8)
-        Me.GroupBox1.Controls.Add(Me.Label13)
         Me.GroupBox1.Controls.Add(Me.Label12)
         Me.GroupBox1.Controls.Add(Me.Label11)
         Me.GroupBox1.Controls.Add(Me.Label10)
@@ -116,10 +100,8 @@ Partial Class AddSavingsDeposit
         Me.GroupBox1.Controls.Add(Me.TextBox6)
         Me.GroupBox1.Controls.Add(Me.TextBox5)
         Me.GroupBox1.Controls.Add(Me.Label8)
-        Me.GroupBox1.Controls.Add(Me.ComboBox1)
         Me.GroupBox1.Controls.Add(Me.TextBox4)
         Me.GroupBox1.Controls.Add(Me.Label7)
-        Me.GroupBox1.Controls.Add(Me.Label6)
         Me.GroupBox1.Controls.Add(Me.TextBox1)
         Me.GroupBox1.Controls.Add(Me.DateTimePicker1)
         Me.GroupBox1.Controls.Add(Me.TextBox2)
@@ -131,64 +113,6 @@ Partial Class AddSavingsDeposit
         Me.GroupBox1.Size = New System.Drawing.Size(763, 289)
         Me.GroupBox1.TabIndex = 12
         Me.GroupBox1.TabStop = False
-        '
-        'Label21
-        '
-        Me.Label21.AutoSize = True
-        Me.Label21.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label21.ForeColor = System.Drawing.Color.Black
-        Me.Label21.Location = New System.Drawing.Point(343, 65)
-        Me.Label21.Name = "Label21"
-        Me.Label21.Size = New System.Drawing.Size(39, 9)
-        Me.Label21.TabIndex = 40
-        Me.Label21.Text = "( ex. 400 )"
-        '
-        'TextBox11
-        '
-        Me.TextBox11.Location = New System.Drawing.Point(323, 77)
-        Me.TextBox11.Name = "TextBox11"
-        Me.TextBox11.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox11.TabIndex = 39
-        '
-        'Label22
-        '
-        Me.Label22.AutoSize = True
-        Me.Label22.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label22.ForeColor = System.Drawing.Color.Black
-        Me.Label22.Location = New System.Drawing.Point(253, 82)
-        Me.Label22.Name = "Label22"
-        Me.Label22.Size = New System.Drawing.Size(64, 15)
-        Me.Label22.TabIndex = 38
-        Me.Label22.Text = "Penalties :"
-        '
-        'Label20
-        '
-        Me.Label20.AutoSize = True
-        Me.Label20.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label20.ForeColor = System.Drawing.Color.Black
-        Me.Label20.Location = New System.Drawing.Point(535, 64)
-        Me.Label20.Name = "Label20"
-        Me.Label20.Size = New System.Drawing.Size(52, 9)
-        Me.Label20.TabIndex = 37
-        Me.Label20.Text = "( ex. DELAY )"
-        '
-        'TextBox10
-        '
-        Me.TextBox10.Location = New System.Drawing.Point(515, 76)
-        Me.TextBox10.Name = "TextBox10"
-        Me.TextBox10.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox10.TabIndex = 36
-        '
-        'Label19
-        '
-        Me.Label19.AutoSize = True
-        Me.Label19.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label19.ForeColor = System.Drawing.Color.Black
-        Me.Label19.Location = New System.Drawing.Point(452, 81)
-        Me.Label19.Name = "Label19"
-        Me.Label19.Size = New System.Drawing.Size(57, 15)
-        Me.Label19.TabIndex = 35
-        Me.Label19.Text = "Remark :"
         '
         'Button3
         '
@@ -207,7 +131,7 @@ Partial Class AddSavingsDeposit
         Me.Label14.AutoSize = True
         Me.Label14.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label14.ForeColor = System.Drawing.Color.Black
-        Me.Label14.Location = New System.Drawing.Point(435, 177)
+        Me.Label14.Location = New System.Drawing.Point(435, 107)
         Me.Label14.Name = "Label14"
         Me.Label14.Size = New System.Drawing.Size(43, 9)
         Me.Label14.TabIndex = 34
@@ -230,7 +154,7 @@ Partial Class AddSavingsDeposit
         Me.Label15.AutoSize = True
         Me.Label15.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label15.ForeColor = System.Drawing.Color.Black
-        Me.Label15.Location = New System.Drawing.Point(343, 178)
+        Me.Label15.Location = New System.Drawing.Point(343, 108)
         Me.Label15.Name = "Label15"
         Me.Label15.Size = New System.Drawing.Size(70, 9)
         Me.Label15.TabIndex = 33
@@ -253,7 +177,7 @@ Partial Class AddSavingsDeposit
         Me.Label16.AutoSize = True
         Me.Label16.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label16.ForeColor = System.Drawing.Color.Black
-        Me.Label16.Location = New System.Drawing.Point(239, 177)
+        Me.Label16.Location = New System.Drawing.Point(239, 107)
         Me.Label16.Name = "Label16"
         Me.Label16.Size = New System.Drawing.Size(64, 9)
         Me.Label16.TabIndex = 32
@@ -264,7 +188,7 @@ Partial Class AddSavingsDeposit
         Me.Label18.AutoSize = True
         Me.Label18.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label18.ForeColor = System.Drawing.Color.Black
-        Me.Label18.Location = New System.Drawing.Point(136, 177)
+        Me.Label18.Location = New System.Drawing.Point(136, 107)
         Me.Label18.Name = "Label18"
         Me.Label18.Size = New System.Drawing.Size(59, 9)
         Me.Label18.TabIndex = 31
@@ -272,7 +196,7 @@ Partial Class AddSavingsDeposit
         '
         'TextBox9
         '
-        Me.TextBox9.Location = New System.Drawing.Point(433, 190)
+        Me.TextBox9.Location = New System.Drawing.Point(433, 120)
         Me.TextBox9.Name = "TextBox9"
         Me.TextBox9.Size = New System.Drawing.Size(50, 20)
         Me.TextBox9.TabIndex = 30
@@ -294,17 +218,6 @@ Partial Class AddSavingsDeposit
         Me.TextBox8.Name = "TextBox8"
         Me.TextBox8.Size = New System.Drawing.Size(50, 20)
         Me.TextBox8.TabIndex = 28
-        '
-        'Label13
-        '
-        Me.Label13.AutoSize = True
-        Me.Label13.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label13.ForeColor = System.Drawing.Color.Black
-        Me.Label13.Location = New System.Drawing.Point(131, 119)
-        Me.Label13.Name = "Label13"
-        Me.Label13.Size = New System.Drawing.Size(49, 9)
-        Me.Label13.TabIndex = 24
-        Me.Label13.Text = "( ex. CASH )"
         '
         'Label12
         '
@@ -352,21 +265,21 @@ Partial Class AddSavingsDeposit
         '
         'TextBox7
         '
-        Me.TextBox7.Location = New System.Drawing.Point(327, 190)
+        Me.TextBox7.Location = New System.Drawing.Point(327, 120)
         Me.TextBox7.Name = "TextBox7"
         Me.TextBox7.Size = New System.Drawing.Size(100, 20)
         Me.TextBox7.TabIndex = 19
         '
         'TextBox6
         '
-        Me.TextBox6.Location = New System.Drawing.Point(221, 190)
+        Me.TextBox6.Location = New System.Drawing.Point(221, 120)
         Me.TextBox6.Name = "TextBox6"
         Me.TextBox6.Size = New System.Drawing.Size(100, 20)
         Me.TextBox6.TabIndex = 18
         '
         'TextBox5
         '
-        Me.TextBox5.Location = New System.Drawing.Point(115, 189)
+        Me.TextBox5.Location = New System.Drawing.Point(115, 119)
         Me.TextBox5.Name = "TextBox5"
         Me.TextBox5.Size = New System.Drawing.Size(100, 20)
         Me.TextBox5.TabIndex = 17
@@ -376,19 +289,11 @@ Partial Class AddSavingsDeposit
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label8.ForeColor = System.Drawing.Color.Black
-        Me.Label8.Location = New System.Drawing.Point(2, 190)
+        Me.Label8.Location = New System.Drawing.Point(2, 120)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(107, 15)
         Me.Label8.TabIndex = 16
         Me.Label8.Text = "Collector's Name :"
-        '
-        'ComboBox1
-        '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(111, 131)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(103, 21)
-        Me.ComboBox1.TabIndex = 15
         '
         'TextBox4
         '
@@ -404,20 +309,9 @@ Partial Class AddSavingsDeposit
         Me.Label7.ForeColor = System.Drawing.Color.Black
         Me.Label7.Location = New System.Drawing.Point(2, 82)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(83, 15)
+        Me.Label7.Size = New System.Drawing.Size(98, 15)
         Me.Label7.TabIndex = 13
-        Me.Label7.Text = "Amount Paid :"
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.ForeColor = System.Drawing.Color.Black
-        Me.Label6.Location = New System.Drawing.Point(2, 132)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(106, 15)
-        Me.Label6.TabIndex = 12
-        Me.Label6.Text = "Payment Method :"
+        Me.Label7.Text = "Amount-Depost :"
         '
         'TextBox1
         '
@@ -523,12 +417,6 @@ Partial Class AddSavingsDeposit
     Friend WithEvents Panel2 As Panel
     Private WithEvents Label23 As Label
     Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents Label21 As Label
-    Friend WithEvents TextBox11 As TextBox
-    Friend WithEvents Label22 As Label
-    Friend WithEvents Label20 As Label
-    Friend WithEvents TextBox10 As TextBox
-    Friend WithEvents Label19 As Label
     Friend WithEvents Button3 As Button
     Friend WithEvents Label14 As Label
     Friend WithEvents Button2 As Button
@@ -539,7 +427,6 @@ Partial Class AddSavingsDeposit
     Friend WithEvents TextBox9 As TextBox
     Friend WithEvents Label17 As Label
     Friend WithEvents TextBox8 As TextBox
-    Friend WithEvents Label13 As Label
     Friend WithEvents Label12 As Label
     Friend WithEvents Label11 As Label
     Friend WithEvents Label10 As Label
@@ -548,10 +435,8 @@ Partial Class AddSavingsDeposit
     Friend WithEvents TextBox6 As TextBox
     Friend WithEvents TextBox5 As TextBox
     Friend WithEvents Label8 As Label
-    Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents TextBox4 As TextBox
     Friend WithEvents Label7 As Label
-    Friend WithEvents Label6 As Label
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents DateTimePicker1 As DateTimePicker
     Friend WithEvents TextBox2 As TextBox
