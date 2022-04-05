b3d1d10f94ead2e94551afa00078bf4e885a221b
RaymartSalaysay <salaysayraymartbs@gmail.com>
2022-04-05 14:04:51 +0800

    wait

--- Fundpuhunan/Savings/WithdrawSavings.Designer.vb
+++ Fundpuhunan/Savings/WithdrawSavings.Designer.vb
@@ -22,32 +22,21 @@ Partial Class WithdrawSavings
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Label9 = New System.Windows.Forms.Label()
         Me.Label10 = New System.Windows.Forms.Label()
         Me.Label12 = New System.Windows.Forms.Label()
         Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(140, 82)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(97, 13)
-        Me.Label2.TabIndex = 110
-        Me.Label2.Text = "------------------------------"
-        '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(143, 121)
+        Me.TextBox2.Location = New System.Drawing.Point(133, 79)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.ReadOnly = True
         Me.TextBox2.Size = New System.Drawing.Size(118, 20)
@@ -59,7 +48,7 @@ Partial Class WithdrawSavings
         Me.Label9.BackColor = System.Drawing.Color.Transparent
         Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label9.ForeColor = System.Drawing.Color.Black
-        Me.Label9.Location = New System.Drawing.Point(174, 109)
+        Me.Label9.Location = New System.Drawing.Point(164, 67)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(49, 9)
         Me.Label9.TabIndex = 108
@@ -71,7 +60,7 @@ Partial Class WithdrawSavings
         Me.Label10.BackColor = System.Drawing.Color.Transparent
         Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label10.ForeColor = System.Drawing.Color.Black
-        Me.Label10.Location = New System.Drawing.Point(15, 121)
+        Me.Label10.Location = New System.Drawing.Point(5, 79)
         Me.Label10.Name = "Label10"
         Me.Label10.Size = New System.Drawing.Size(122, 15)
         Me.Label10.TabIndex = 107
@@ -82,7 +71,7 @@ Partial Class WithdrawSavings
         Me.Label12.AutoSize = True
         Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label12.ForeColor = System.Drawing.Color.Black
-        Me.Label12.Location = New System.Drawing.Point(174, 144)
+        Me.Label12.Location = New System.Drawing.Point(164, 102)
         Me.Label12.Name = "Label12"
         Me.Label12.Size = New System.Drawing.Size(47, 9)
         Me.Label12.TabIndex = 105
@@ -90,7 +79,7 @@ Partial Class WithdrawSavings
         '
         'TextBox4
         '
-        Me.TextBox4.Location = New System.Drawing.Point(143, 156)
+        Me.TextBox4.Location = New System.Drawing.Point(133, 114)
         Me.TextBox4.Name = "TextBox4"
         Me.TextBox4.Size = New System.Drawing.Size(118, 20)
         Me.TextBox4.TabIndex = 102
@@ -100,22 +89,11 @@ Partial Class WithdrawSavings
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label7.ForeColor = System.Drawing.Color.Black
-        Me.Label7.Location = New System.Drawing.Point(15, 156)
+        Me.Label7.Location = New System.Drawing.Point(5, 114)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(109, 15)
         Me.Label7.TabIndex = 101
         Me.Label7.Text = "Withdraw Amount :"
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.ForeColor = System.Drawing.Color.Black
-        Me.Label5.Location = New System.Drawing.Point(15, 80)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(39, 15)
-        Me.Label5.TabIndex = 100
-        Me.Label5.Text = "Date :"
         '
         'Label1
         '
@@ -144,7 +122,7 @@ Partial Class WithdrawSavings
         Me.Button3.BackColor = System.Drawing.Color.SteelBlue
         Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button3.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Button3.Location = New System.Drawing.Point(233, 194)
+        Me.Button3.Location = New System.Drawing.Point(223, 146)
         Me.Button3.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(98, 34)
@@ -157,7 +135,7 @@ Partial Class WithdrawSavings
         Me.Button2.BackColor = System.Drawing.SystemColors.ActiveBorder
         Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button2.ForeColor = System.Drawing.SystemColors.ButtonFace
-        Me.Button2.Location = New System.Drawing.Point(124, 194)
+        Me.Button2.Location = New System.Drawing.Point(114, 146)
         Me.Button2.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(101, 34)
@@ -169,17 +147,15 @@ Partial Class WithdrawSavings
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(334, 234)
+        Me.ClientSize = New System.Drawing.Size(326, 185)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.Label9)
         Me.Controls.Add(Me.Label10)
         Me.Controls.Add(Me.Label12)
         Me.Controls.Add(Me.TextBox4)
         Me.Controls.Add(Me.Label7)
-        Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Label6)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
@@ -193,15 +169,12 @@ Partial Class WithdrawSavings
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents Label2 As Label
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Label9 As Label
     Friend WithEvents Label10 As Label
     Friend WithEvents Label12 As Label
     Friend WithEvents TextBox4 As TextBox
     Friend WithEvents Label7 As Label
-    Friend WithEvents Label5 As Label
     Friend WithEvents Label1 As Label
     Friend WithEvents Label6 As Label
     Friend WithEvents Button3 As Button
